411b42930d8f7b8c1a4a595cbad6659845a9f45f
Dewalle Julien <julien.dewalle@hotmail.fr>
2020-01-23 14:36:41 +0100

    CSS OK

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+movies;
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-}
+
+  ionViewWillEnter() { 
+    this.http
+    .get('https://api.themoviedb.org/3/movie/550?api_key=f022620a05af8fe4fa6be3e95ed6345e')
+    .toPromise()
+    .then(movies => this.movies = movies);
+    };
+  }
